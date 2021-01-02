139ae33717f3b4d217cf6986269753c8bdeade49
csxiaodiao <i@xiaodiao.me>
2021-01-02 15:59:41 +0800

    fx

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -2,7 +2,10 @@ class WechatsController < ApplicationController
   # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#wechat_responder---rails-responder-controller-dsl
   wechat_responder
 
-  on :text do |request, content|
-    request.reply.text "echo: #{content}" # Just echo
+
+
+  on :event, with: 'subscribe' do |request|
+    request.reply.text '<a href="http://wx.bestpv.cn/card">百万诊疗金免费领取</a>'
   end
+
 end
