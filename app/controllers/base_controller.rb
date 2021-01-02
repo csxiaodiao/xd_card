aa969333da2316b76d99827232a09a3c27d5b5a3
csxiaodiao <i@xiaodiao.me>
2021-01-02 13:37:18 +0800

    fx

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class BaseController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
-  def current_user
-    User.first
-  end
 
 end
