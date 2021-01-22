9fdd84e5ef0fa04fa38476ecc17b45d4972218a2
csxiaodiao <i@xiaodiao.me>
2021-01-22 20:41:58 +0800

    fx

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -2,12 +2,10 @@ class WechatsController < ApplicationController
   # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#wechat_responder---rails-responder-controller-dsl
   wechat_responder
 
-
-
   on :event, with: 'subscribe' do |request|
     request.reply.text '终于等到您，还好我没放弃~
 现代口腔感谢您的关注和支持！
-<a href="http://wx.bestpv.cn/card">百万诊疗金领取</a>👈
+<a href="http://wx.bestpv.cn/activities/2/card">寒假矫正基金领取</a>👈
 【医院地址/咨询热线】
 新碶门诊：北仑区新碶新大路308号
 咨询热线：0574-86832226
@@ -16,9 +14,4 @@ class WechatsController < ApplicationController
 大碶门诊：北仑区大碶人民南路79号
 咨询热线：0574-86115999'
   end
-
 end
-
-
-
-
