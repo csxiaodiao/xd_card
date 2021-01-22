b11df6434d6d2658c1e089dad93c579c27797f2d
csxiaodiao <i@xiaodiao.me>
2021-01-22 19:32:41 +0800

    fx

--- app/controllers/my/cards_controller.rb
+++ app/controllers/my/cards_controller.rb
@@ -2,29 +2,27 @@
 
 class My::CardsController < BaseController
   wechat_api
-  before_action :find_card, only: [:show, :edit, :update]
+  before_action :find_card, only: %i[show edit update]
   def index
     @cards = current_user.can_use_cards
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    code = "1111"
-    if params[:v_code] != "666666"
-      msg = "核销码不正确，请询问店员确认"
+    code = '1111'
+    if params[:v_code] != '666666'
+      msg = '核销码不正确，请询问店员确认'
       flash[:alert] = msg
       return redirect_back fallback_location: :back
     end
 
     if @card.finished?
-      msg = "此券已使用"
+      msg = '此券已使用'
       flash[:alert] = msg
-       return redirect_back fallback_location: :back
+      return redirect_back fallback_location: :back
     end
 
     begin
@@ -35,23 +33,19 @@ class My::CardsController < BaseController
         @user_card.save!
       end
       redirect_to success_my_card_path(@card)
-    rescue => exception
-      msg = "核销失败,未知错误"
+    rescue StandardError => e
+      msg = '核销失败,未知错误'
       flash[:alert] = msg
-      return redirect_back fallback_location: :back
+      redirect_back fallback_location: :back
     end
-
-
   end
 
-  def success
-  end
+  def success; end
 
   private
 
   def find_card
     @user_card = current_user.user_cards.find_by_card_id(params[:id])
-    @card = @user_card.card
+    @card = @user_card.card.includes(:activity)
   end
-
 end
