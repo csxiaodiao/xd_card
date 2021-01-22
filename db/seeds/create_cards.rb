5f68e524117a3fe5bdf30e5bddfeab01b70e6532
csxiaodiao <i@xiaodiao.me>
2021-01-22 19:23:44 +0800

    fx

--- db/seeds/create_cards.rb
+++ db/seeds/create_cards.rb
@@ -1,40 +1,11 @@
 # frozen_string_literal: true
 
-UserCard.destroy_all
-Card.destroy_all
-
-activity = Activity.create(name: 'hd1', start_at: '2021-01-22 08:00:00', end_at: '2021-02-01 08:00:00', status: 1)
-
+activity = Activity.last
 items = [
-  # er
-  { name: '儿牙诊疗金', desc_name: '儿童预成冠', kind: 'er', cost: 15, sku: 300 },
-  { name: '儿牙诊疗金', desc_name: '儿童预成冠', kind: 'er', cost: 28, sku: 250 },
-  { name: '儿牙诊疗金', desc_name: '儿童预成冠', kind: 'er', cost: 35, sku: 200 },
-  { name: '儿牙诊疗金', desc_name: '儿童预成冠', kind: 'er', cost: 45, sku: 150 },
-  { name: '儿牙诊疗金', desc_name: '儿童预成冠', kind: 'er', cost: 50, sku: 100 },
 
-  { name: '全瓷冠诊疗金', desc_name: '全瓷冠', kind: 'xf', cost: 188, sku: 325 },
-  { name: '全瓷冠诊疗金', desc_name: '全瓷冠', kind: 'xf', cost: 208, sku: 275 },
-  { name: '全瓷冠诊疗金', desc_name: '全瓷冠', kind: 'xf', cost: 388, sku: 250 },
-  { name: '全瓷冠诊疗金', desc_name: '全瓷冠', kind: 'xf', cost: 500, sku: 50 },
-
-  { name: '种植诊疗金', desc_name: '种植', kind: 'zz', cost: 580, sku: 325 },
-  { name: '种植诊疗金', desc_name: '种植', kind: 'zz', cost: 680, sku: 275 },
-  { name: '种植诊疗金', desc_name: '种植', kind: 'zz', cost: 780, sku: 250 },
-  { name: '种植诊疗金', desc_name: '种植', kind: 'zz', cost: 880, sku: 100 },
-  { name: '种植诊疗金', desc_name: '种植', kind: 'zz', cost: 980, sku: 50 },
-
-  { name: '传统矫正诊疗金', desc_name: '传统矫正', kind: 'zj', cost: 880, sku: 300 },
-  { name: '传统矫正诊疗金', desc_name: '传统矫正', kind: 'zj', cost: 980, sku: 250 },
-  { name: '国产时代天使诊疗金', desc_name: '国产时代天使', kind: 'zj', cost: 1680, sku: 210 },
-  { name: '国产时代天使诊疗金', desc_name: '国产时代天使', kind: 'zj', cost: 2000, sku: 160 },
-  { name: '进口隐适美隐形矫正诊疗金', desc_name: '进口隐适美隐形矫正', kind: 'zj', cost: 2680, sku: 50 },
-  { name: '进口隐适美隐形矫正诊疗金', desc_name: '进口隐适美隐形矫正', kind: 'zj', cost: 3000, sku: 30 },
-
-  { name: '美白诊疗金', desc_name: '美白', kind: 'mb', cost: 88, sku: 350 },
-  { name: '美白诊疗金', desc_name: '美白', kind: 'mb', cost: 128, sku: 350 },
-  { name: '美白诊疗金', desc_name: '美白', kind: 'mb', cost: 228, sku: 250 },
-  { name: '美白诊疗金', desc_name: '美白', kind: 'mb', cost: 300, sku: 200 }
+  { name: '传统矫正基金', desc_name: '传统矫正', kind: 'ctjz', cost: 1000, sku: 2000 },
+  { name: '国产时代天使矫正基金', desc_name: '国产时代天使矫正', kind: 'sdts', cost: 2000, sku: 2000 },
+  { name: '进口隐适美隐形矫正基金', desc_name: '进口隐适美隐形矫正', kind: 'ysm', cost: 3000, sku: 2000 }
 
 ]
 
@@ -49,7 +20,7 @@ items.each do |item|
       kind: item[:kind],
       use: "凭此券在北仑现代口腔任意一家门诊消费#{item[:desc_name]}项目可抵扣现金",
       explain: '本券不可与门诊内其他优惠同享,每人限领1张,过期作废',
-      expiration_date: '2021-01-31 23:59:59',
+      expiration_date: '2021-02-28 23:59:59',
       activity_id: activity.id,
       created_at: Time.now,
       updated_at: Time.now
